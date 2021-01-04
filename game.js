c60e3627e214db236882cfa44f69c6c4065ee662
Cliff0317 <59384254+Cliff0317@users.noreply.github.com>
2021-01-04 17:38:54 +0800

    Add files via upload

--- game.js
+++ game.js
@@ -3,26 +3,53 @@ function rand(max) {
 }
 let round = 1;
 let check = false;
-let score = 1;
+let score = 10;
+let guess = 1;
+var output = document.getElementById("output")
 function see() {
-    if (score == 0) {
-        document.getElementById("result").textContent = "Jake：您已經失去了員工的信任！ 那我為什麼要相信你？"
-        document.getElementById("score").textContent = "Jake：您投資了" + round + "輪。但是，我們將解僱您，並找到一個更聰明的老闆...祝您好運！Bye！"
-        location.href = "https://cliff0317.github.io/GamblerFallacy/gameover.htm"
-    }
-    if (check) {
-        document.getElementById("result").textContent = "Jake：對！您的話是對的！"
-        score += 1
-        document.getElementById("score").textContent = "員工信任度：" + score + "，您投資了" + round + "輪。"
-        round += 1
+    var res = "第" + round + "次嘗試，結果" + (check ? ("正確") : ("錯誤")) + "，猜測" + (guess ? ("股價上漲") : ("股價下跌")) + "。<br>"
+    output.innerHTML += res
+    if (round > 10) {
+        if (score == 0) {
+            document.getElementById("result").textContent = "您已經失去了員工的信任！ 那我為什麼要相信你？"
+            document.getElementById("score").textContent = "您投資了" + round + "輪。但是，我們將解僱您，並找到一個更聰明的老闆...祝您好運！"
+            alert("遊戲結束。 請把分數報告給教授。");
+            return
+        }
+        if (check) {
+            document.getElementById("result").textContent = "對！您的話是對的！"
+            score += 1
+            document.getElementById("score").textContent = "員工信任度：" + score + "，您投資了" + round + "輪。"
+            round += 1
+        } else {
+            document.getElementById("result").textContent = "不！您的話似乎不正確！下一次，請稍微調整一下猜測！"
+            score -= 1
+            document.getElementById("score").textContent = "員工信任度：" + score + "，您投資了" + round + "輪。"
+            round += 1
+        }
     } else {
-        document.getElementById("result").textContent = "Jake：不！您的話似乎不正確！下一次，請稍微調整一下猜測！"
-        score -= 1
-        document.getElementById("score").textContent = "員工信任度：" + score + "，您投資了" + round + "輪。"
-        round += 1
+        if (round == 10) {
+            document.getElementById("result").textContent = "現在，您即將在我們公司工作！我覺得您是“得分王”！"
+            score = 10
+            round += 1
+            return
+        }
+        if (check) {
+            document.getElementById("result").textContent = "對！您的話是對的！"
+            score += 1
+            document.getElementById("score").textContent = "員工信任度：" + score + "，您投資了" + round + "輪。"
+            round += 1
+        } else {
+            document.getElementById("result").textContent = "不！您的話似乎不正確！下一次，請稍微調整一下猜測！"
+            score -= 1
+            document.getElementById("score").textContent = "員工信任度：" + score + "，您投資了" + round + "輪。"
+            round += 1
+        }
     }
+    window.scrollTo(0,document.body.scrollHeight);
 }
 function rise() {
+    guess = 1;
     let ans = rand(2);
     if (ans == 1) {
         check = true;
@@ -32,6 +59,7 @@ function rise() {
     see()
 }
 function fall() {
+    guess = 0;
     let ans = rand(2);
     if (ans == 0) {
         check = true;
