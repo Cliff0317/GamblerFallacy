de6737c69273aeb97c88904e0b5d943596c71580
Cliff0317 <59384254+Cliff0317@users.noreply.github.com>
2021-01-06 16:09:06 +0800

    Update game.js

--- game.js
+++ game.js
@@ -60,6 +60,7 @@ function see() {
             document.getElementById("result").textContent = "現在，您即將在我們公司工作！我覺得您是“得分王”！"
             score = defaultScore;
             round += 1
+            document.getElementById("score").textContent = "您即將在我們公司工作！你能行的！"
             return
         }
         if (check) {
